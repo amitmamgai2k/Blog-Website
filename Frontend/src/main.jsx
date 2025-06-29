aa07e6b8866495dcbdaf5e54bcdd589b44b726f1
amitmamgai2k <123645217+amitmamgai2k@users.noreply.github.com>
2025-06-29 11:23:05 +0530

    .

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,20 +1,12 @@
-
-import './index.css'
-import ReactDOM from 'react-dom/client'
-import {Toaster} from 'react-hot-toast'
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import App from './App.jsx'
-
-import store from './Redux/store.js'
+import './index.css';
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-<BrowserRouter>
-
+  <BrowserRouter>
     <App />
-    <Toaster/>
-
+    <Toaster />
   </BrowserRouter>
-  </Provider>
-)
+);
